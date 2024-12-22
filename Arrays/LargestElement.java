2ce4f47600b432bae05d971ae2761a0ae56e2128
sabeen2 <sabeenpandey2@gmail.com>
2024-12-23 00:08:28 +0545

    recursive

--- Arrays/LargestElement.java
+++ Arrays/LargestElement.java
@@ -4,8 +4,10 @@ import java.util.Arrays;
 public class LargestElement {
     public static void main(String[] args) {
         int[] arr = new int[]{1,11,6,5,3,2,3};
+       
 
         System.out.println(largest(arr));
+        System.out.println(largestRecursive(arr,  0, 0));
 
         
     }
@@ -21,4 +23,17 @@ public class LargestElement {
         
         return res;
     }
+
+
+//recursive approach
+public static int largestRecursive(int[] arr,  int res, int index) {
+    int len = arr.length;
+    if(len==index) return res; 
+
+    if(res<arr[index]) res = arr[index];
+    
+     return largestRecursive(arr,res,index+1);
+  
+}
+
 }
