81a94d87e1367fc86e862ce3bc444fec82437b0a
sabeen2 <sabeenpandey2@gmail.com>
2024-12-23 10:50:36 +0545

    optimal for second smallest and largest algo

--- Arrays/SecondSmallestAndLargest.java
+++ Arrays/SecondSmallestAndLargest.java
@@ -4,6 +4,14 @@ public class SecondSmallestAndLargest {
 
      public static void main(String[] args) {
         int[] arr = new int[] {6,1,9,5,3,11,19};
+        // secondSmallestAndLargest(arr);
+        secondSmallestAndLargestOptimal(arr);
+    
+    }
+
+
+    //bruteforce approach : Time - 0(n) + O(n) = O(n)
+    public static void secondSmallestAndLargest(int[] arr) {
         int largest = Integer.MIN_VALUE;
         int smallest = Integer.MAX_VALUE;
         int secondLargest= Integer.MIN_VALUE;
@@ -33,6 +41,39 @@ public class SecondSmallestAndLargest {
     System.out.println(smallest + " smallest"); 
     System.out.println(secondLargest + " secondLargest"); 
     System.out.println(secondSmallest + " secondSmallest"); 
-    
     }
+
+//Optimal apporoach in single traverse: Time - O(n)
+public static void secondSmallestAndLargestOptimal(int[] arr) {
+    int largest = Integer.MIN_VALUE;
+    int smallest = Integer.MAX_VALUE;
+    int secondLargest= Integer.MIN_VALUE;
+    int secondSmallest = Integer.MAX_VALUE;
+    for(int i : arr) {
+        if(i < smallest ) {
+            secondSmallest = smallest;
+            smallest = i;
+           
+        }
+        else if(i<secondSmallest) {
+            secondSmallest = i;
+        }
+        if(i>largest) {
+            secondLargest = largest;
+
+            largest  = i;
+        }
+        else if(i>secondLargest) {
+            secondLargest = i;
+        }
+    }
+
+    System.out.println(largest + " largest"); 
+    System.out.println(smallest + " smallest"); 
+    System.out.println(secondLargest + " secondLargest"); 
+    System.out.println(secondSmallest + " secondSmallest"); 
 }
+
+
+}
+
